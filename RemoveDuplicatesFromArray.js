5d6b1a62a58f21c46ffde454831c676d490b8d58
SpurthiMahadev <144647207+SpurthiMahadev@users.noreply.github.com>
2024-02-08 02:41:41 +0530

    Update RemoveDuplicatesFromArray.js

--- RemoveDuplicatesFromArray.js
+++ RemoveDuplicatesFromArray.js
@@ -7,3 +7,18 @@ const result = [...new Set(arr)]
 // Array.from() creates new array from array like object or iterables.
 
 console.log(result)
+
+// function removeDuplicates(array) {
+//   const uniqueArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (uniqueArray.indexOf(array[i]) === -1) {
+//       uniqueArray.push(array[i]);
+//     }
+//   }
+//   return uniqueArray;
+// }
+
+// const array = [1, 2, 3, 4, 2, 3, 5, 6, 7, 7, 8, 9, 9];
+// const uniqueArray = removeDuplicates(array);
+// console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+

7f2b37b074ae5c18c3b3bf8b0570bc576d31cd00
SpurthiMahadev <144647207+SpurthiMahadev@users.noreply.github.com>
2024-04-20 11:43:09 +0530

    Update findLongestWordInASentence.js

--- findLongestWordInASentence.js
+++ findLongestWordInASentence.js
@@ -1,16 +1,21 @@
-function findLongestWord(sentence) {
-  const words = sentence.split(' ');
-  let longestWord = "";
-
-  words.forEach(word => {
-    if (word.length > longestWord.length) {
-      longestWord = word;
-    }
-  });
-
-  return longestWord;
+const findLongestWord = (testSentence) => {
+const words = testSentence.split(' ')
+let longestWord = []
+let maxLength = 0
+words.forEach(item => {
+let length = item.length
+if( length > maxLength){
+maxLength = length
+longestWord = [item]
+} else if (length === maxLength){
+longestWord.push(item)
 }
+})
+return longestWord
+}
+
+
 
 const testSentence = "The quick brown fox jumps over the lazy dog";
 const longestWord = findLongestWord(testSentence);
-console.log("Longest Word:", longestWord);
+console.log(longestWord);
